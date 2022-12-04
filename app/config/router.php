3941c62a676a36b6f64ce58f9dca7958679cc417
Devdudumuniz <dudumunizads@gmail.com>
2022-12-04 02:41:24 -0300

    commit

--- app/config/router.php
+++ app/config/router.php
@@ -1,21 +1,14 @@
 <?php
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $this->get('/', function(){
+        echo "Home!!";
+    });
+    $this->get('/home', function(){
+        echo "Estou na Home!!";
+    });
+    $this->get('/about', function(){
+        echo "estou na bout!!";
+    });
 
 
 
