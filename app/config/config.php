0921a918ab2cfaaabada851048685c3198006021
Dudu Muniz <dudumunizads@gmail.com>
2022-12-03 23:18:03 -0300

    primeiro commit

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+//constantes
+define('BASE','/mvc');
+define('UNSET_URI_COUNT', 1);
+define('DEBUG_URI', true);
+
+
+
+
+
+?>
---
+++ app/functions/functions.php
@@ -0,0 +1,16 @@
+<?php
+function add($params = [], $die = true){
+    echo '<pre>';
+    print_r($params);
+    echo '</pre>';
+
+    if($die) die();
+}
+
+
+
+
+
+
+
+?>
